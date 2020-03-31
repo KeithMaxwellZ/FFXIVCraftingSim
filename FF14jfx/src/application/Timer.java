08422d4ad24953cb8eb7b9df362dd61593231833
keith <keith@DESKTOP-HDGL1VO>
2020-03-31 17:29:08 -0400

    created timer

---
+++ FF14jfx/src/application/Timer.java
@@ -0,0 +1,36 @@
+package application;
+
+public class Timer
+{
+	private long startTime;
+	private long endTime;
+	private Status st;
+	
+	public Timer() {
+		startTime = 0;
+		endTime = 0;
+		st = Status.stopped;
+	}
+	
+	public void startTimer() {
+		startTime = System.currentTimeMillis();
+		st = Status.started;
+	}
+	
+	public void stopTimer() {
+		endTime = System.currentTimeMillis();
+		st = Status.stopped;
+	}
+	
+	public double getTime() {
+		if(st == Status.stopped) {
+			return (double)(endTime - startTime) / 1000.0;
+		} else {
+			return 0;
+		}
+	}
+}
+
+enum Status{
+	started, stopped
+}
